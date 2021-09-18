9938e4829cfae2b532175d38b97af4c97bcf2a56
GitHub Action <action@github.com>
2021-09-18 03:48:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/toyflivver/ideapluginstarter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.toyflivver.ideapluginstarter.services
+
+import com.github.toyflivver.ideapluginstarter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
